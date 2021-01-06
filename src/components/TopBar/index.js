e9c35b527fbc667eeff7ea0905186ed2439b7070
Damjan Piperski <damjanpiperski@gmail.com>
2021-01-06 22:08:31 +0100

    Fetch and axios connecting for public api usage

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -1,8 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import './style.scss';
 import ThemeSwitch from '../ThemeSwitch';
+import axios from "axios";
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://exchange.bitcoin/api/trades?since=5',
+// };
+
+// axios.request(options).then(function (response) {
+//   console.log(response.data);
+// }).catch(function (error) {
+//   console.error(error);
+// });
+
+axios({
+  method: 'GET',
+  url: 'https://exchange.bitcoin/api/trades?since=5',
+}).then((response) =>console.log(response.data)).catch((err) => console.log(err))
 
 const TopBar = () => {
+  fetch('https://api.github.com/users/hacktivist123/repos')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+
   const [appear, setAppear] = useState(false);
   const [longerAppear, setLongerAppear] = useState(false);
   useEffect(() => {
