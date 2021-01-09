e39e82595d82d5e582431988870d510938b3efdf
Damjan Piperski <damjanpiperski@gmail.com>
2021-01-10 00:36:13 +0100

    Fetch to random api

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -3,9 +3,21 @@ import './style.scss';
 import ThemeSwitch from '../ThemeSwitch';
 
 const TopBar = () => {
-  fetch('https://exchange.bitcoin/api/trades?since=5')
-  .then(response => response.json())
+  //ispod je alpha vantage, ali samo 5Xmin, 500/dan tako da pazi
+  // const ApiKey = '7ZL096TZKZGMLBXY';
+
+  // fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${ApiKey}`)
+  // .then(response => response.json())
+  // .then(data => console.log(data))
+
+
+  // ispod je random user, odlican za paginaciju i dodatni load na scroll, i za neki dobar ui
+  fetch('https://randomuser.me/api/')
+  .then(res => res.json())
   .then(data => console.log(data));
+
+
+  // ideja, magicna kocka za pitanja (api za yes/no) \\\ onda graph od dobijenih odgovora, predvidjanje za DA i NE posle toga? tako nesto
 
   const [appear, setAppear] = useState(false);
   const [longerAppear, setLongerAppear] = useState(false);
